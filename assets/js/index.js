436c2ac33870c7d98a6915edd2884e067df4bb82
unknown <griffinhthomas12@gmail.com>
2023-10-13 15:24:43 -0500

    added array items

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 // Functions only needed for the main page (index.html)
+
+[	
+  {	
+  "ingredients": [	
+    "4.5 cl (3 parts) vodka",
+    "9 cl (6 parts) Tomato juice",
+    "1.5 cl (1 part) Lemon juice",
+    "2 to 3 dashes of Worcestershire Sauce",
+    "Tabasco sauce",
+    "Celery salt",
+    "Black pepper"
+  ],	
+  "instructions": "Stirring gently, pour all ingredients into highball glass. Garnish.",	
+  "name": "bloody mary"	
+  "picture": "url"	
+  }	
+  ]	
